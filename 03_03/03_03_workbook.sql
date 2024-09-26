9d37c980401ee9373a5752445b5561fe46afdd35
maggieindata <104745444+maggieindata@users.noreply.github.com>
2024-09-26 23:17:09 +0000

    set up workbooks to follow along

--- 03_03/03_03_workbook.sql
+++ 03_03/03_03_workbook.sql
@@ -3,7 +3,3 @@
 ----     listing these times in descending order from the busiest to the least busy, 
 ----     along with the corresponding bike trip counts. 
 -- Hint: GROUP_BY(), ORDER_BY()
-select half_hour_starttime, count(*) as trip_count
-from public.trip_data 
-group by half_hour_starttime
-order by trip_count desc;
